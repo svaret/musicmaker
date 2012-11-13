7642019247488a8b74dbb0676e7b38a40c0e2817
Lasse Bergström <lasse@nextit.se>
2012-11-13 20:17:03 +0100

    Added lost files

---
+++ grails-app/controllers/musicmaker/MusicController.groovy
@@ -0,0 +1,15 @@
+package musicmaker
+
+class MusicController {
+    private static final int INTRO_NOTES = 2
+    private static final int NUMBER_OF_CHORDS_IN_VERSE = 5
+    private static final int NUMBER_OF_CHORDS_IN_CHORUS = 3
+    private static final int NUMBER_OF_NOTES_IN_OUTRO = 12
+
+    def index() {
+        Song song = new Song(INTRO_NOTES, NUMBER_OF_CHORDS_IN_VERSE, NUMBER_OF_CHORDS_IN_CHORUS, NUMBER_OF_NOTES_IN_OUTRO);
+        def songInView = song.getCompleteSongStringHtmlFormat();
+        [songInView: songInView]
+    }
+
+}
