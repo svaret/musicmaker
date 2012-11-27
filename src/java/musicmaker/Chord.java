d10f2880e0075230caf9fde9e52797d24d6c98c6
Lasse Bergström <lasse@nextit.se>
2012-11-27 21:02:31 +0100

    Formatting of Tone and Chord Enums + making descriptive field final.

--- src/java/musicmaker/Chord.java
+++ src/java/musicmaker/Chord.java
@@ -6,20 +6,11 @@ import java.util.List;
 import java.util.Random;
 
 public enum Chord {
-    C, C_MINOR("Cm"),
-    C_SHARP("C#"), C_SHARP_MINOR("C#m"),
-    D, D_MINOR("Dm"),
-    D_SHARP("D#"), D_SHARP_MINOR("D#m"), 
-    E, E_MINOR("Em"),
-    F, F_MINOR("Fm"),
-    F_SHARP("F#"), F_SHARP_MINOR("F#m"), 
-    G, G_MINOR("Gm"),
-    G_SHARP("G#"), G_SHARP_MINOR("G#m"), 
-    A, A_MINOR("Am"),
-    A_SHARP("A#"), A_SHARP_MINOR("A#m"), 
-    B, B_MINOR("Bm");
+    C, C_MINOR("Cm"), C_SHARP("C#"), C_SHARP_MINOR("C#m"), D, D_MINOR("Dm"), D_SHARP("D#"), D_SHARP_MINOR("D#m"),
+    E, E_MINOR("Em"), F, F_MINOR("Fm"), F_SHARP("F#"), F_SHARP_MINOR("F#m"), G, G_MINOR("Gm"), G_SHARP("G#"),
+    G_SHARP_MINOR("G#m"), A, A_MINOR("Am"), A_SHARP("A#"), A_SHARP_MINOR("A#m"), B, B_MINOR("Bm");
 
-    private String label;
+    private final String label;
     private static final List<Chord> CHORDS = Collections.unmodifiableList(Arrays.asList(values()));
     private static final int SIZE = CHORDS.size();
     private static final Random RANDOM = new Random();
--- src/java/musicmaker/Tone.java
+++ src/java/musicmaker/Tone.java
@@ -6,20 +6,9 @@ import java.util.List;
 import java.util.Random;
 
 public enum Tone {
-    C,
-    C_SHARP("C#"),
-    D,
-    D_SHARP("D#"),
-    E,
-    F,
-    F_SHARP("F#"),
-    G,
-    G_SHARP("G#"),
-    A(),
-    A_SHARP ("A#"),
-    B();
+    C, C_SHARP("C#"), D, D_SHARP("D#"), E, F, F_SHARP("F#"), G, G_SHARP("G#"), A(), A_SHARP("A#"), B();
 
-    private String tone;
+    private final String tone;
     private static final List<Tone> TONES = Collections.unmodifiableList(Arrays.asList(values()));
     private static final int SIZE = TONES.size();
     private static final Random RANDOM = new Random();
