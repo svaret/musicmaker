73357bc8a7deec5264dc6710f5ceacf304f423f6
Lasse Bergström <lasse@nextit.se>
2012-12-25 21:07:24 +0100

    Removing redundant code.

--- grails-app/controllers/musicmaker/SongController.groovy
+++ grails-app/controllers/musicmaker/SongController.groovy
@@ -2,7 +2,6 @@ package musicmaker
 
 import grails.converters.JSON
 import grails.converters.XML
-import musicmaker.factory.SongHtmlFactory
 
 class SongController {
     private static final int NUMBER_OF_NOTES_IN_INTRO = 2
@@ -14,22 +13,8 @@ class SongController {
         def song = new musicmakerr.Song(NUMBER_OF_NOTES_IN_INTRO, NUMBER_OF_CHORDS_IN_VERSE,
                 NUMBER_OF_CHORDS_IN_CHORUS, NUMBER_OF_NOTES_IN_OUTRO);
         withFormat {
-            html { [song: SongHtmlFactory.createCompleteSongHtml(NUMBER_OF_NOTES_IN_INTRO,
-                    NUMBER_OF_CHORDS_IN_VERSE, NUMBER_OF_CHORDS_IN_CHORUS, NUMBER_OF_NOTES_IN_OUTRO)] }
             xml { render song as XML }
             json { render song.intro as JSON }
         }
     }
-
-    def list = {
-        def song = new Song(NUMBER_OF_NOTES_IN_INTRO, NUMBER_OF_CHORDS_IN_VERSE, NUMBER_OF_CHORDS_IN_CHORUS,
-                NUMBER_OF_NOTES_IN_OUTRO);
-        withFormat {
-            html { [song: song.createCompleteSongStringHtmlFormat()] }
-            xml { render song as XML }
-            json { render song.intro as JSON }
-        }
-    }
-
-
 }
