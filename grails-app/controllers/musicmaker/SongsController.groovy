f75a057107126cf901e96bcc0560a76accfc08f7
Lasse Bergström <lasse@nextit.se>
2013-03-03 12:09:47 +0100

    Static import of synchronize and small formatting

--- grails-app/controllers/musicmaker/SongsController.groovy
+++ grails-app/controllers/musicmaker/SongsController.groovy
@@ -4,6 +4,8 @@ import com.mongodb.BasicDBObject
 import grails.converters.JSON
 import org.bson.types.ObjectId
 
+import static com.mongodb.util.JSON.serialize
+
 class SongsController {
     private static final int NUMBER_OF_NOTES_IN_INTRO = 2
     private static final int NUMBER_OF_CHORDS_IN_VERSE = 5
@@ -17,7 +19,7 @@ class SongsController {
         def db = mongo.getDB(MUSICMAKER_DB)
         BasicDBObject dbObject = new BasicDBObject(request.JSON)
         db.song.insert(dbObject)
-        ObjectId songId = (ObjectId)dbObject.get( "_id" )
+        ObjectId songId = (ObjectId) dbObject.get("_id")
 
         def result = [status: "OK", songId: songId.toString()]
         render result as JSON
@@ -27,14 +29,14 @@ class SongsController {
         def id = params.id
         def db = mongo.getDB(MUSICMAKER_DB)
         if (id != null)
-            render com.mongodb.util.JSON.serialize(db.song.findOne(_id: new ObjectId(id)))
+            render serialize(db.song.findOne(_id: new ObjectId(id)))
         else
-            render com.mongodb.util.JSON.serialize(db.song.find())
+            render serialize(db.song.find())
     }
 
     def random() {
         Song song = new Song(NUMBER_OF_NOTES_IN_INTRO, NUMBER_OF_CHORDS_IN_VERSE,
-                NUMBER_OF_CHORDS_IN_CHORUS, NUMBER_OF_NOTES_IN_OUTRO, "Author" )
+                NUMBER_OF_CHORDS_IN_CHORUS, NUMBER_OF_NOTES_IN_OUTRO, "Author")
         SongView songView = new SongView(song)
 
         render songView as JSON
@@ -43,21 +45,21 @@ class SongsController {
     def update() {
         def id = params.id
         def db = mongo.getDB(MUSICMAKER_DB)
-        db.song.update([_id: new ObjectId(id)],
-                [$set: request.JSON])
+        db.song.update([_id: new ObjectId(id)], [$set: request.JSON])
 
-		def result = [status: "OK"]
-		render result as JSON
-	}
+        def result = [status: "OK"]
+        render result as JSON
+    }
 
-	def delete() {
-		def id = params.id
-		def db = mongo.getDB(MUSICMAKER_DB)
-		if (id == null)
-			db.song.remove([:])
-		else
-			db.song.remove(_id: new ObjectId(id))
-		def result = [status: "OK"]
-		render result as JSON
-	}
+    def delete() {
+        def id = params.id
+        def db = mongo.getDB(MUSICMAKER_DB)
+        if (id == null)
+            db.song.remove([:])
+        else
+            db.song.remove(_id: new ObjectId(id))
+
+        def result = [status: "OK"]
+        render result as JSON
+    }
 }
