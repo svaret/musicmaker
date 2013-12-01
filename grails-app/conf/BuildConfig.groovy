512ed7e898df4a26086d116f32f70afc8b6bc88c
Lasse Bergström <lasse@nextit.se>
2013-12-01 12:36:39 +0100

    Updgraded resources plugin

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -46,7 +46,7 @@ grails.project.dependency.resolution = {
     plugins {
         runtime ':mongodb:1.3.0'
         runtime ":jquery:1.10.2"
-        runtime ":resources:1.2"
+        runtime ":resources:1.2.1"
 
 
         // Uncomment these (or add new ones) to enable additional resources capabilities
