5b3e50374e94c23a64a27b38ef89cd40d30b066e
supergillie <peter.krooks@gmail.com>
2014-01-17 15:44:05 +0100

    Some sentence logic from last week committed after a fight in the team about food.

--- grails-app/domain/musicmaker/Sentence.groovy
+++ grails-app/domain/musicmaker/Sentence.groovy
@@ -2,12 +2,22 @@ package musicmaker
 
 class Sentence {
     List<Word> words = []
-
-    WordArchive wordArchive = new WordArchive()
+    private WordArchive wordArchive = new WordArchive()
+    public String wordString = ""
 
     Sentence() {
         (0..5).each {
             words << wordArchive.randomWord
         }
+
+        for (int i; i< this.words.size(); i++){
+            wordString = wordString +" "+ words[i].content
+        }
+        String firstLetter
+        firstLetter = (String) wordString.charAt(1)
+        String restOfString = wordString.substring(2)
+
+        String firstLetterInCapital = (String) Character.toUpperCase(firstLetter.toCharacter())
+        firstLetterInCapital + restOfString + "."
     }
 }
--- test/unit/musicmaker/SentenceTest.groovy
+++ test/unit/musicmaker/SentenceTest.groovy
@@ -1,13 +1,28 @@
 package musicmaker
 
+import org.junit.Test
+import musicmaker.Sentence
 import spock.lang.Specification
 
-//@TestFor(WordClass)
+//@TestFor(Sentence)
 class SentenceTest extends Specification {
 
     def setup() {
     }
 
+    @Test
+    def "getTextSencentce should return string consisting of 5 words equals 4 blanks"(){
+        given:
+
+        when:
+        Sentence sentence = new Sentence()
+
+        then:
+        sentence.wordString.contains(" ")       //TODO: Den här kollar ju bara på ett balksteg!
+
+    }
+
+    @Test
     def "When creating a Sentence expect a result containing 6 words. "() {
         given:
 
@@ -17,11 +32,25 @@ class SentenceTest extends Specification {
         then:
         sentence.words.size == 6
 
-        (0..5).each {
-           it in Word
-        }
+
+
     }
+
+    @Test
+    def "PWhen creating a Sentence the sentence should contian capital first letter and point at the end. "() {
+        given:
+
+        when:
+        Sentence sentence = new Sentence()
+
+        then:
+        char c = sentence.wordString[sentence.wordString.length()]
+        c == "."
+
+    }
+
 }
+
 
 
 
